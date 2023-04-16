f3ee0220cf94a8879ae4a3e80d6ffdbe55cf3184
Bogdan Patseev <patseev1987@gmail.com>
2023-04-16 10:19:03 +0300

    First commit

---
+++ src/Lesson1/sample1/Sample.java
@@ -0,0 +1,14 @@
+package Lesson1.sample1;
+
+import Lesson1.Product;
+
+public class Sample {
+
+    public static void main(String[] args) {
+
+        Product product = new Product();
+
+
+    }
+
+}
